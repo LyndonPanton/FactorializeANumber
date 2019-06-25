f635172c2b1514e0f1bd63bfcff37949cfeda2ed
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-25 17:45:29 +0100

    Added input event listener to input field

--- js/app.js
+++ js/app.js
@@ -3,5 +3,8 @@
 window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
-	
+	let input = document.getElementById("number");
+	input.addEventListener("input", function(event) {
+
+	});
 }
