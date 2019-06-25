46d471052e6dc7f060d2a5ed0986f3257e460090
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-25 17:50:01 +0100

    Added function for calculating factorials

--- js/app.js
+++ js/app.js
@@ -3,8 +3,12 @@
 window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function factorialize(number) {
+		
+	}
+
 	let input = document.getElementById("number");
 	input.addEventListener("input", function(event) {
-
+		factorialize(Number(event.target.value));
 	});
 }
