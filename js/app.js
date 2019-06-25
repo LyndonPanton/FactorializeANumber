0f501dc08aedc89374ac3582ef4eb827bebd54b8
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-25 18:08:11 +0100

    Added errorneous input validation

--- js/app.js
+++ js/app.js
@@ -4,11 +4,22 @@ window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function factorialize(number) {
-		
+		let factorial = 0;
+
+		if (isNaN(number)) {
+			return "NaN";
+		} else if ((Number(number)) < 1) {
+			return "Number must be more than 0";
+		} else if (Number(number) % 1 !== 0 || number.indexOf("e") !== -1) {
+			return "Number must be an integer";
+		} else {
+			console.log(4);
+			
+		}
 	}
 
 	let input = document.getElementById("number");
 	input.addEventListener("input", function(event) {
-		factorialize(Number(event.target.value));
+		factorialize(event.target.value);
 	});
 }
